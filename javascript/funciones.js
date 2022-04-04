9158a207f154c56b95d396c74b0f97c599cd7f43
sol peskin <psolchu@gmail.com>
2022-04-03 22:25:28 -0300

    agrego js

---
+++ javascript/funciones.js
@@ -0,0 +1,52 @@
+// arrays :
+let comentariosFooter = [];
+let preguntasHechas = [];
+
+// clases
+class preguntas {
+
+    constructor (pregunta, respuesta){
+        this.pregunta = pregunta
+        this.respuesta = respuesta
+    }
+}
+
+// guardar comentarios del footer en variable
+function GuardarComentario(){
+    let comentarios = document.getElementsByClassName("comentarios").value;
+
+    if (comentarios !== ""){
+        console.log (comentarios);
+        comentariosFooter.push (comentarios);
+    }
+}
+
+function CrearPreguntas(pregunta, respuesta){
+    elementPreg =  new preguntas (pregunta, respuesta);
+
+    preguntasHechas.push (elementPreg);
+}
+
+// son ejemplos random porque todavía no se cuáles van a ser las preguntas 
+CrearPreguntas(
+    "¿De que color es el cielo?",
+    "azul"
+)
+CrearPreguntas(
+    "¿como me llamo?",
+    "yo me llamo sol"
+)
+
+function Buscar(){
+    let preguntaBuscada = document.getElementsByClassName("buscarPregunta").value; /*como no me funciona, para probarlo uso el prompt*/
+    preguntaBuscada = prompt ("cuál pregunta");
+
+    preguntasHechas.forEach((pregunta) => {
+        if (pregunta.pregunta.indexOf (preguntaBuscada) !==-1){
+            console.log (pregunta.respuesta);
+
+        }
+    } )
+
+}
+
